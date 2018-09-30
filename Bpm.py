2e8cc1cff493223cafe9da5169003aa5a8f716e4
Uamhan <31971768+Uamhan@users.noreply.github.com>
2018-09-30 18:28:34 +0100

    fixed layout of bpm

--- Bpm.py
+++ Bpm.py
@@ -1,10 +1,7 @@
 import librosa 
 
-
-# read audio file 
-x, sr = librosa.load('test.mp3') 
-
-
-tempo, beat_times = librosa.beat.beat_track(x, sr=sr, start_bpm=60, units='time')
-
-print(tempo)
+#takes path of audio file and returns Bpm of audio
+def getBpm(path):
+    x, sr = librosa.load(path) 
+    tempo, _ = librosa.beat.beat_track(x, sr=sr, start_bpm=60, units='time')
+    return tempo
